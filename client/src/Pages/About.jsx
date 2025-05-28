76795e9b41fd9ee15216f958175dd829186f16bc
Charles <charlecharle070@gmail.com>
2025-05-28 12:09:17 +0530

    about commited

--- client/src/Pages/About.jsx
+++ client/src/Pages/About.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
 
-export const About = () => {
+import React from 'react';
+
+function About() {
   return (
-    <div>About</div>
-      
-     
-  )
+    <div>
+      <h1>About This Project</h1>
+      <p>This is a simple React project created by me.</p>
+
+      <h2>Purpose</h2>
+      <p>I built this project to practice my React skills and understand how components work.</p>
+
+      <h2>What I Used</h2>
+      <ul>
+        <li>React</li>
+        <li>JavaScript</li>
+        <li>No CSS (yet!)</li>
+      </ul>
+
+      <h2>About Me</h2>
+      <p>I’m dhanush a Cyber Security student learning Web Development.</p>
+    </div>
+  );
 }
+
+export default About;
